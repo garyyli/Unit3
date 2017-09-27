81a72ce8d68397b5ec86ea2205e215ec963dd33a
garyyli <garyli@hanovernorwichschools.org>
2017-09-27 13:44:24 -0400

    Updated from Brython Server: 9/27/2017 1:44:25 PM

--- loopDemo.py
+++ loopDemo.py
@@ -24,15 +24,21 @@ while i<=20:
 #print out the odd numbers from 13 to 27
 for i in range(13,28,2):
         print(i)"""
-        
+"""        
 i = 13
 while i<=27:
     print(i)
-    i = i+2
-
+    i = i+2"""
 """
 #add up the numbers from 1 to 5
 total = 0
 for i in range(1,6):
     total = total + i
 print(total)"""
+total = 0
+i = 1
+while i<=5:
+    total = total + i
+    print(i)
+    i = i+1
+    
