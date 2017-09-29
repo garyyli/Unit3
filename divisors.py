ae0c5e9a20183d5f11c91a0bcd9ca19bf13245e8
garyyli <garyli@hanovernorwichschools.org>
2017-09-29 14:43:53 -0400

    Updated from Brython Server: 9/29/2017 2:43:53 PM

--- divisors.py
+++ divisors.py
@@ -1,1 +1,11 @@
+#Gary Li
+#9/29/17
+#divisors.py
 
+number = int(input('Enter a number: '))
+
+for i in range (0,number+1):
+    if number%number = 0:
+        print(number)
+    number = number -1
+    
