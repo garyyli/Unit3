75ee04dcd201499e49ae594e269b41ecf40a2bf0
garyyli <garyli@hanovernorwichschools.org>
2017-10-02 14:31:41 -0400

    Updated from Brython Server: 10/2/2017 2:31:41 PM

--- perfectNumber.py
+++ perfectNumber.py
@@ -8,7 +8,7 @@ total = 0
 for i in range(1,number):
     if number%i==0:
         total = total+i
-    if total = number:
-        print('Perfect')
-    else:
-        print('Not perfect')
+if total == number:
+    print('Perfect')
+else:
+    print('Not perfect')
