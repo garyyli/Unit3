ba2540c4dc9b713d1af3125e14e1817df62a8e4c
garyyli <garyli@hanovernorwichschools.org>
2017-10-02 14:51:22 -0400

    Updated from Brython Server: 10/2/2017 2:51:22 PM

--- fri13.py
+++ fri13.py
@@ -7,25 +7,20 @@ day = date.today().day
 month = date.today().month
 year = date.today().year
 print(month, day, year)
+
 friday13 = 0
+
 if day>13 and month!=12:
     month=month+1
-else:
-    month==1
-    year= year+1
-if friday13<=10:
-    weekday = weekday(year,mont,13)
-    if weekday ==5:
+
+while friday13<=10:
+    weekday = weekday(year,month,13)
+    if weekday ==4:
         friday13 = friday13+1
         print(month, '13', year)
-        if month!=12:
-            month+ month+1
-        else month == 1
-        year = year + 1
+    if month!=12:
+        month= month +1
     else:
-        if month!=12:
-            month= month +1
-        else:
-            month == 1
-            year = year + 1
-print(weekday(year, month, day))
+        month = 1
+        year = year + 1
+    
