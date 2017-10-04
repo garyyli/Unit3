9b3ce944f78fb96928f6a04f85b29861ab273d92
garyyli <garyli@hanovernorwichschools.org>
2017-10-04 13:43:58 -0400

    Updated from Brython Server: 10/4/2017 1:44:00 PM

--- warmup9.py
+++ warmup9.py
@@ -1,1 +1,19 @@
+#Gary Li
+#10/4/17
+#warmup9.py
 
+word = input('Enter a word: ')
+
+for ch in word:
+    if ch = a:
+        print(ch.upper())
+    if ch = e:
+        print(ch.upper())
+    if ch = i:
+        print(ch.upper())
+    if ch = o:
+        print(ch.upper())
+    if ch = u:
+        print(ch.upper())
+    else:
+        print(ch)
