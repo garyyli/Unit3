7e9d5eb3a6bc47c1b6296dce05444b6e32ba2235
garyyli <garyli@hanovernorwichschools.org>
2017-10-05 14:38:51 -0400

    Updated from Brython Server: 10/5/2017 2:38:52 PM

--- warmup10.py
+++ warmup10.py
@@ -1,1 +1,13 @@
+#Gary Li
+#10/5/17
+#warmup10.py
 
+from ggame import *
+blue=Color(0xFF0,1)
+
+triangle1 = PolygonAsset([(0,0), (120,180), (60,300)],LineStyle(1,blue), blue)
+
+for j in range(10):
+    for i in range(10):
+        Sprite(triangle1,(20+50*i,20+50*j))
+App().run()
