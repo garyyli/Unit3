7ca3bad139ec103ee0c0e30a5246595f8a6b2181
garyyli <garyli@hanovernorwichschools.org>
2017-10-05 14:43:33 -0400

    Updated from Brython Server: 10/5/2017 2:43:34 PM

--- warmup10.py
+++ warmup10.py
@@ -4,10 +4,13 @@
 
 from ggame import *
 blue=Color(0xFF0,1)
+red=Color(0xFFF000,1)
 
-triangle1 = PolygonAsset([(0,0), (120,180), (60,300)],LineStyle(1,blue), blue)
+triangle1 = PolygonAsset([(0,0), (80,0), (40,40)],LineStyle(1,blue), blue)
+triangle2 = PolygonAsset([(0,100), (80,-40), (40,0)],LineStyle(1,red), red)
 
 for j in range(10):
     for i in range(10):
-        Sprite(triangle1,(20+50*i,20+50*j))
+        Sprite(triangle1,(20+70*i,20+70*j))
+        Sprite(triangle2,(20+70*i,20+70*j))
 App().run()
